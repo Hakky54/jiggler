882b6bdbf9538ec1f135beb7a8993de873312942
Hakky54 <hakangoudberg@hotmail.com>
2022-12-23 13:44:22 +0100

    Added runners for native executables

---
+++ script/runner-for-linux-amd64.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+getBinaries() {
+  curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.0/graalvm-ce-java17-linux-amd64-22.3.0.tar.gz | tar -xzf - -C bin/graalvm --strip-components=1
+  curl -sL https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz | tar -xzf - -C bin/maven --strip-components=1
+}
+
+sudo apt-get update && sudo apt-get install build-essential libz-dev zlib1g-dev -y
+mkdir -p bin/graalvm
+mkdir -p bin/maven
+getBinaries
+JAVA_HOME=`pwd`"/bin/graalvm"
+bin/graalvm/bin/gu install native-image
+bin/maven/bin/mvn clean install -Pnative-image
+cp target/jiggler4j jiggler4j
+tar -czf bin/jiggler4j-linux-amd64.tar.gz jiggler4j
+rm jiggler4j
