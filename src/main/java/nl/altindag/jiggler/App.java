c09bc6486294b796f9fc5c3954503ea4147735b0
Hakky54 <hakangoudberg@hotmail.com>
2023-01-19 21:51:23 +0100

    Removed graalvm picoli configuration

--- src/main/java/nl/altindag/jiggler/App.java
+++ src/main/java/nl/altindag/jiggler/App.java
@@ -1,20 +1,45 @@
+/*
+ * Copyright 2022 Thunderberry.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package nl.altindag.jiggler;
 
-import nl.altindag.jiggler.command.JiggleCommand;
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Robot;
+import java.util.concurrent.TimeUnit;
 
-@Command(
-        name = "jiggler",
-        version = "jiggler v1.0.0",
-        description = "CLI tool to jiggle your mouse"
-)
 public class App {
 
-    public static void main(String[] applicationArguments) {
-        new CommandLine(new JiggleCommand())
-                .setCaseInsensitiveEnumValuesAllowed(true)
-                .execute(applicationArguments);
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        int[][] positions = {
+                {2, 0, -2, 0},
+                {0, 2, 0, -2}
+        };
+
+        Robot robot = new Robot();
+        while (true) {
+            for (int i = 0; i < 4; i++) {
+                Point currentPoint = MouseInfo.getPointerInfo().getLocation();
+                int pointX = currentPoint.x + positions[0][i];
+                int pointy = currentPoint.y + positions[1][i];
+                robot.mouseMove(pointX, pointy);
+
+                TimeUnit.SECONDS.sleep(2);
+            }
+        }
     }
 
 }
